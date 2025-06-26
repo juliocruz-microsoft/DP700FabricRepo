995cd26f69835cba1a8c0f89f1953d1327863dad
Julio Cruz <63519981+juliocruz-microsoft@users.noreply.github.com>
2025-06-26 06:50:55 -0700

    Committing 14 items from workspace ee3a5fc4-745c-4a5c-9b6e-434382b18a89

---
+++ Fabric/DP700DWH.Warehouse/dbo/Tables/DimCustomer.sql
@@ -0,0 +1,17 @@
+CREATE TABLE [dbo].[DimCustomer] (
+
+	[CustomerKey] int NOT NULL, 
+	[CustomerAltKey] varchar(50) NULL, 
+	[Title] varchar(5) NULL, 
+	[FirstName] varchar(50) NOT NULL, 
+	[LastName] varchar(50) NULL, 
+	[AddressLine1] varchar(200) NULL, 
+	[City] varchar(50) NULL, 
+	[StateProvince] varchar(50) NULL, 
+	[CountryRegion] varchar(50) NULL, 
+	[PostalCode] varchar(20) NULL
+);
+
+
+GO
+ALTER TABLE [dbo].[DimCustomer] ADD CONSTRAINT UQ_934fda91_f101_47eb_9a83_3a4a016ac3e3 unique NONCLUSTERED ([CustomerKey]);
---
+++ Fabric/Delta Notebook.Notebook/notebook-content.sql
@@ -0,0 +1,414 @@
+-- Fabric notebook source
+
+-- METADATA ********************
+
+-- META {
+-- META   "kernel_info": {
+-- META     "name": "synapse_pyspark"
+-- META   },
+-- META   "dependencies": {
+-- META     "lakehouse": {
+-- META       "default_lakehouse": "a02606ef-fb66-481d-ad2a-45747d7275e8",
+-- META       "default_lakehouse_name": "DP700Lakehouse",
+-- META       "default_lakehouse_workspace_id": "ee3a5fc4-745c-4a5c-9b6e-434382b18a89",
+-- META       "known_lakehouses": [
+-- META         {
+-- META           "id": "a02606ef-fb66-481d-ad2a-45747d7275e8"
+-- META         }
+-- META       ]
+-- META     }
+-- META   }
+-- META }
+
+-- CELL ********************
+
+CREATE TABLE employees
+  (id INT, name STRING, salary DOUBLE);
+
+-- METADATA ********************
+
+-- META {
+-- META   "language": "sparksql",
+-- META   "language_group": "synapse_pyspark"
+-- META }
+
+-- CELL ********************
+
+select * from employees
+
+-- METADATA ********************
+
+-- META {
+-- META   "language": "sparksql",
+-- META   "language_group": "synapse_pyspark"
+-- META }
+
+-- CELL ********************
+
+describe detail employees
+
+-- METADATA ********************
+
+-- META {
+-- META   "language": "sparksql",
+-- META   "language_group": "synapse_pyspark"
+-- META }
+
+-- CELL ********************
+
+describe history employees
+
+-- METADATA ********************
+
+-- META {
+-- META   "language": "sparksql",
+-- META   "language_group": "synapse_pyspark"
+-- META }
+
+-- CELL ********************
+
+INSERT INTO employees
+VALUES 
+  (1, "Adam", 3500.0),
+  (2, "Sarah", 4020.5);
+
+INSERT INTO employees
+VALUES
+  (3, "John", 2999.3),
+  (4, "Thomas", 4000.3);
+
+INSERT INTO employees
+VALUES
+  (5, "Anna", 2500.0);
+
+INSERT INTO employees
+VALUES
+  (6, "Kim", 6200.3)
+
+-- METADATA ********************
+
+-- META {
+-- META   "language": "sparksql",
+-- META   "language_group": "synapse_pyspark"
+-- META }
+
+-- CELL ********************
+
+select * from employees
+
+-- METADATA ********************
+
+-- META {
+-- META   "language": "sparksql",
+-- META   "language_group": "synapse_pyspark"
+-- META }
+
+-- CELL ********************
+
+describe detail employees
+
+-- METADATA ********************
+
+-- META {
+-- META   "language": "sparksql",
+-- META   "language_group": "synapse_pyspark"
+-- META }
+
+-- CELL ********************
+
+describe history employees
+
+-- METADATA ********************
+
+-- META {
+-- META   "language": "sparksql",
+-- META   "language_group": "synapse_pyspark"
+-- META }
+
+-- CELL ********************
+
+UPDATE employees 
+SET salary = salary + 10000
+WHERE name LIKE "A%"
+
+-- METADATA ********************
+
+-- META {
+-- META   "language": "sparksql",
+-- META   "language_group": "synapse_pyspark"
+-- META }
+
+-- CELL ********************
+
+select * from employees
+
+-- METADATA ********************
+
+-- META {
+-- META   "language": "sparksql",
+-- META   "language_group": "synapse_pyspark"
+-- META }
+
+-- CELL ********************
+
+DESCRIBE history employees
+
+-- METADATA ********************
+
+-- META {
+-- META   "language": "sparksql",
+-- META   "language_group": "synapse_pyspark"
+-- META }
+
+-- CELL ********************
+
+DESCRIBE detail employees
+
+-- METADATA ********************
+
+-- META {
+-- META   "language": "sparksql",
+-- META   "language_group": "synapse_pyspark"
+-- META }
+
+-- CELL ********************
+
+select * from employees version as of 1
+
+-- METADATA ********************
+
+-- META {
+-- META   "language": "sparksql",
+-- META   "language_group": "synapse_pyspark"
+-- META }
+
+-- CELL ********************
+
+delete from employees
+
+-- METADATA ********************
+
+-- META {
+-- META   "language": "sparksql",
+-- META   "language_group": "synapse_pyspark"
+-- META }
+
+-- CELL ********************
+
+select * from employees
+
+-- METADATA ********************
+
+-- META {
+-- META   "language": "sparksql",
+-- META   "language_group": "synapse_pyspark"
+-- META }
+
+-- CELL ********************
+
+describe history employees
+
+-- METADATA ********************
+
+-- META {
+-- META   "language": "sparksql",
+-- META   "language_group": "synapse_pyspark"
+-- META }
+
+-- CELL ********************
+
+restore table employees to version as of 4
+
+-- METADATA ********************
+
+-- META {
+-- META   "language": "sparksql",
+-- META   "language_group": "synapse_pyspark"
+-- META }
+
+-- CELL ********************
+
+select * from employees
+
+-- METADATA ********************
+
+-- META {
+-- META   "language": "sparksql",
+-- META   "language_group": "synapse_pyspark"
+-- META }
+
+-- CELL ********************
+
+optimize employees
+ZORDER by id
+
+-- METADATA ********************
+
+-- META {
+-- META   "language": "sparksql",
+-- META   "language_group": "synapse_pyspark"
+-- META }
+
+-- CELL ********************
+
+describe detail employees
+
+-- METADATA ********************
+
+-- META {
+-- META   "language": "sparksql",
+-- META   "language_group": "synapse_pyspark"
+-- META }
+
+-- CELL ********************
+
+VACUUM employees
+
+-- METADATA ********************
+
+-- META {
+-- META   "language": "sparksql",
+-- META   "language_group": "synapse_pyspark"
+-- META }
+
+-- CELL ********************
+
+describe history employees
+
+-- METADATA ********************
+
+-- META {
+-- META   "language": "sparksql",
+-- META   "language_group": "synapse_pyspark"
+-- META }
+
+-- CELL ********************
+
+VACUUM employees RETAIN 0 HOURS
+
+-- METADATA ********************
+
+-- META {
+-- META   "language": "sparksql",
+-- META   "language_group": "synapse_pyspark"
+-- META }
+
+-- CELL ********************
+
+
+-- METADATA ********************
+
+-- META {
+-- META   "language": "sparksql",
+-- META   "language_group": "synapse_pyspark"
+-- META }
+
+-- CELL ********************
+
+set spark.databricks.delta.retentionDurationCheck.enabled = false;
+
+-- METADATA ********************
+
+-- META {
+-- META   "language": "sparksql",
+-- META   "language_group": "synapse_pyspark"
+-- META }
+
+-- CELL ********************
+
+CREATE TABLE managed_default
+  (width INT, length INT, height INT);
+
+
+
+
+
+-- METADATA ********************
+
+-- META {
+-- META   "language": "sparksql",
+-- META   "language_group": "synapse_pyspark"
+-- META }
+
+-- CELL ********************
+
+INSERT INTO managed_default
+VALUES (3 INT, 2 INT, 1 INT)
+
+-- METADATA ********************
+
+-- META {
+-- META   "language": "sparksql",
+-- META   "language_group": "synapse_pyspark"
+-- META }
+
+-- CELL ********************
+
+Select * from managed_default
+
+-- METADATA ********************
+
+-- META {
+-- META   "language": "sparksql",
+-- META   "language_group": "synapse_pyspark"
+-- META }
+
+-- CELL ********************
+
+delete 
+
+-- METADATA ********************
+
+-- META {
+-- META   "language": "sparksql",
+-- META   "language_group": "synapse_pyspark"
+-- META }
+
+-- CELL ********************
+
+DROP table managed_default
+
+-- METADATA ********************
+
+-- META {
+-- META   "language": "sparksql",
+-- META   "language_group": "synapse_pyspark"
+-- META }
+
+-- CELL ********************
+
+drop table external_default
+
+-- METADATA ********************
+
+-- META {
+-- META   "language": "sparksql",
+-- META   "language_group": "synapse_pyspark"
+-- META }
+
+-- CELL ********************
+
+CREATE TABLE external_default
+  (width INT, length INT, height INT)
+LOCATION 'Files/external_default';
+  
+INSERT INTO external_default
+VALUES (3 INT, 2 INT, 1 INT)
+
+-- METADATA ********************
+
+-- META {
+-- META   "language": "sparksql",
+-- META   "language_group": "synapse_pyspark"
+-- META }
+
+-- CELL ********************
+
+select * from external_default
+
+-- METADATA ********************
+
+-- META {
+-- META   "language": "sparksql",
+-- META   "language_group": "synapse_pyspark"
+-- META }
