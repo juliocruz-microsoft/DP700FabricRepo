490e006deeb12d5f183752e389baa7b3e6c44507
Julio Cruz <63519981+juliocruz-microsoft@users.noreply.github.com>
2025-10-02 07:09:49 -0700

    Committing 17 items from workspace f371512d-3806-4888-9fc4-a407e39710ae

---
+++ DimEmployee_Silver.Notebook/notebook-content.py
@@ -0,0 +1,46 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "557bcfcd-744d-4a21-a76c-98d7672aadd0",
+# META       "default_lakehouse_name": "DP700Lakehouse",
+# META       "default_lakehouse_workspace_id": "f371512d-3806-4888-9fc4-a407e39710ae",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "557bcfcd-744d-4a21-a76c-98d7672aadd0"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+df = spark.read.format("csv").option("header","true").load("Files/bronze/DimEmployeebronze.csv")
+# df now is a Spark DataFrame containing CSV data from "Files/bronze/DimEmployeebronze.csv".
+display(df)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+from pyspark.sql.functions import *
+df.write.format("delta").mode("overwrite").saveAsTable("DimEmployee")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
