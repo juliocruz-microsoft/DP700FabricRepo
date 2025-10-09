34945e17f75c117845bffc98230bc533d1e8acc1
Julio Cruz <63519981+juliocruz-microsoft@users.noreply.github.com>
2025-10-09 05:58:35 -0700

    Committing 16 items from workspace 6175e975-10b1-48af-972a-63d46f8148cd

---
+++ JCDP700Warehouse.Warehouse/dbo/Tables/DimEmployee.sql
@@ -0,0 +1,9 @@
+CREATE TABLE [dbo].[DimEmployee] (
+
+	[EmployeeKey] int NULL, 
+	[Full Name] varchar(8000) NULL, 
+	[Nome] varchar(8000) NULL, 
+	[Sobrenome] varchar(8000) NULL, 
+	[Title] varchar(8000) NULL, 
+	[EmailAddress] varchar(8000) NULL
+);
